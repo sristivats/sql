980f7244506673edbfdc55629231a0346797b374
maidevalhoon <devalsinghal355@gmail.com>
2025-05-28 22:48:13 +0530

    pulled the data debug some bugs

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,12 +1,16 @@
+CREATE DATABASE retail_db;
+USE retail_db;
 -- Drop tables if they already exist (in reverse dependency order)
 DROP TABLE IF EXISTS Inventory;
 DROP TABLE IF EXISTS Products;
-DROP TABLE IF EXISTS Stores;
 DROP TABLE IF EXISTS Staff;
+DROP TABLE IF EXISTS Stores;
+DROP TABLE IF EXISTS Inventory_Staging;
+
 
 -- Stores table
 CREATE TABLE Stores (
-    store_id INT PRIMARY KEY,
+    store_id VARCHAR(4) PRIMARY KEY,
     region VARCHAR(100)
 );
 
@@ -14,13 +18,13 @@ CREATE TABLE Stores (
 CREATE TABLE Staff (
     staff_id INT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    store_id INT,
+    store_id VARCHAR(4),
     FOREIGN KEY (store_id) REFERENCES Stores(store_id)
 );
 
 -- Products table
 CREATE TABLE Products (
-    product_id INT PRIMARY KEY,
+    product_id VARCHAR(5) PRIMARY KEY,
     category VARCHAR(100),
     price FLOAT,
     seasonality VARCHAR(100)
@@ -30,12 +34,12 @@ CREATE TABLE Products (
 CREATE TABLE Inventory (
     inventory_id INT PRIMARY KEY AUTO_INCREMENT,
     date DATE NOT NULL,
-    store_id INT,
-    product_id INT,
+    store_id VARCHAR(4),
+    product_id VARCHAR(5),
     inventory_level INT,
     units_sold INT,
     units_ordered INT,
-    demand_forecast INT,
+    demand_forecast FLOAT,
     discount FLOAT,
     competitor_pricing FLOAT,
     holiday_promotion BOOLEAN,
@@ -47,14 +51,14 @@ CREATE TABLE Inventory (
 
 CREATE TABLE Inventory_Staging (
     date DATE,
-    store_id INT,
-    product_id INT,
+    store_id VARCHAR(4),
+    product_id VARCHAR(5),
     category VARCHAR(100),
     region VARCHAR(100),
     inventory_level INT,
     units_sold INT,
     units_ordered INT,
-    demand_forecast INT,
+    demand_forecast FLOAT,
     price FLOAT,
     discount FLOAT,
     weather_condition VARCHAR(100),
@@ -63,10 +67,9 @@ CREATE TABLE Inventory_Staging (
     seasonality VARCHAR(100)
 );
 
-LOAD DATA INFILE '../data/inventory_data.csv'
+LOAD DATA LOCAL INFILE './data/inventory_data.csv'
 INTO TABLE Inventory_Staging
 FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (date, store_id, product_id, category, region, inventory_level, units_sold, units_ordered, demand_forecast, price, discount, weather_condition, holiday_promotion, competitor_pricing, seasonality);
